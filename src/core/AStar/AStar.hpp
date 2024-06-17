f74933ce056057c2558ceac1cbb211131928e44d
wolgemoth <25903926+wolgemoth@users.noreply.github.com>
2024-06-17 22:06:48 +0100

    Preliminary Setup

---
+++ src/core/AStar/AStar.hpp
@@ -0,0 +1,16 @@
+#ifndef PATHFINDING_ASTAR_HPP
+#define PATHFINDING_ASTAR_HPP
+
+namespace Pathfinding::Modules::AStar {
+
+    class AStar {
+
+    public:
+
+    private:
+
+    };
+
+} // Pathfinding::Modules::AStar
+
+#endif //PATHFINDING_ASTAR_HPP
---
+++ src/test/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/**
+ * @file main.cpp
+ * @brief Global entry point.
+ */
+int main([[maybe_unused]] int _argc, [[maybe_unused]] char* _argv[]) {
+
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
