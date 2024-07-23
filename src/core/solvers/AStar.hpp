aff4821852ac45f64ed63cea85d362ebe90cdfda
NazarElsayed <nazar.elsayed@gmail.com>
2024-07-23 02:22:00 +0100

    Custom Priority Queue and Test

--- src/core/solvers/AStar.hpp
+++ src/core/solvers/AStar.hpp
@@ -10,24 +10,30 @@
 
 namespace CHDR::Solvers {
 
-    template <typename T>
+    template<typename T>
     class AStar : public ISolver<T> {
 
     private:
 
-    	template <size_t Kd>
-    	struct ASData {
+        template<size_t Kd>
+        struct ASData {
+            float m_GScore;
+            float m_FScore;
 
-    		Coord<size_t, Kd> m_Parent;
+            Coord<size_t, Kd> m_Parent;
 
-    		float m_GScore;
-    		float m_FScore;
+            constexpr ASData(const float& _gScore, const float& _hScore, const Coord<size_t, Kd>& _parent = {}) :
+                m_GScore(_gScore),
+                m_FScore(_gScore + _hScore),
+                m_Parent(_parent) {}
+        };
 
-    		constexpr ASData(const Coord<size_t, Kd>& _parent, const float& _gScore, const float& _fScore) :
-    			m_Parent(_parent),
-    			m_GScore(_gScore),
-    			m_FScore(_fScore) {}
-    	};
+        template<size_t Kd>
+        struct NodeCompare {
+            bool operator()(const ASData<Kd>& _a, const ASData<Kd>& _b) {
+                return _a.m_FScore > _b.m_FScore;
+            }
+        };
 
     public:
 
@@ -35,135 +41,106 @@ namespace CHDR::Solvers {
             throw std::runtime_error("AStar::Solve(const Mazes::IMaze& _maze) Not implemented!");
         }
 
-        template <size_t Kd>
+        template<size_t Kd>
         auto Solve(const Mazes::Grid<Kd, T>& _maze, const Coord<size_t, Kd>& _start, const Coord<size_t, Kd>& _end) {
+            using data_t  = ASData<Kd>;
+            using coord_t = Coord<size_t, Kd>;
 
-			using data_t = ASData<Kd>;
-        	using coord_t = Coord<size_t, Kd>;
+            std::vector<coord_t> result;
 
-        	std::vector<coord_t> result;
+            std::priority_queue<data_t, std::vector<data_t>, NodeCompare<Kd>> openSet;
 
-			std::unordered_map<coord_t, data_t> openSet;
+            data_t startData(0.0f, EuclideanDistance(_start, _end));
+            data_t data1(17.0f, 10.2f);
+            data_t data2(53.0f, 16.4f);
+            data_t data3(34.0f, 11.8f);
+            data_t data4(21.0f, 53.2f);
 
-			//TODO: The squiglies are bad.
-			data_t startData({}, 0.0f, EuclideanDistance(_start, _end));
-        	std::pair<coord_t, data_t> start = std::make_pair(_start, startData);
+            openSet.push(startData);
+            openSet.push(data1);
+            openSet.push(data2);
+            openSet.push(data3);
+            openSet.push(data4);
 
-        	openSet.insert(start);
+            std::cout << std::endl;
+            std::cout << "Priority queue data" << "\n";
 
-			// Custom comparitor for open set. Orders the set like a min heap.
-			auto min = [](data_t* _A, data_t* _B) { return _A->m_FScore > _B->m_FScore; };
+            const int size = openSet.size();
+            for (int i = 0; i < size; i++) {
+                std::cout << openSet.top().m_FScore << "\n";
+                openSet.pop();
+            }
 
-			// Create a priority queue to contain nodes as we process them.
-			std::priority_queue<data_t, decltype(min)> workingSet(min);
-			workingSet.push(start);
+            std::cout << std::endl;
 
-			/* Solve the path */
-			data_t* curr = nullptr;
+            //std::unordered_map<coord_t, data_t> openSet;
+            //openQueue.push(_start);
 
-			bool solutionFound = false;
+            //std::pair<coord_t, data_t> start = std::make_pair(_start, startData);
 
-			while (workingSet.size() != 0) {
-
-				curr = workingSet.top();
-
-				if (curr == &_end) {
-					solutionFound = true;
-					break;
-				}
-
-				workingSet.pop();
-
-				for (auto& neighbor : curr->m_Neighbors) {
-
-					auto gScore = curr->m_GScore + _distanceHeuristic(*curr, *neighbor);
-
-					if (gScore < neighbor->m_GScore) {
-						neighbor->m_Parent = curr;
-						neighbor->m_GScore = gScore;
-						neighbor->m_FScore = gScore + _distanceHeuristic(*neighbor, _end);
-
-						workingSet.push(neighbor);
-					}
-				}
-			}
-
-			if (solutionFound) {
-
-				while (curr != nullptr) {
-					result.insert(result.end(), curr->m_Position);
-
-					curr = curr->m_Parent;
-				}
-			}
-
-        	return result;
+            //openSet.insert(start);
         }
 
-    	template <size_t Kd>
-		static constexpr auto _distanceHeuristic(const Coord<size_t, Kd>& _A, const Coord<size_t, Kd>& _B) {
-	        return ManhattanDistance(_A, _B);
+        template<size_t Kd>
+        static constexpr auto _distanceHeuristic(const Coord<size_t, Kd>& _A, const Coord<size_t, Kd>& _B) {
+            return ManhattanDistance(_A, _B);
         }
 
 #pragma region Heuristics
-    	
-    	/**
-		 * @brief Computes the Euclidean distance between two nodes.
+
+        /**
+         * @brief Computes the Euclidean distance between two nodes.
          *
-    	 * @tparam Kd The number of dimensions of the nodes.
-    	 * @param _A The first node.
-		 * @param _B The second node.
-		 * @return The Euclidean distance between _A and _B.
-		 */
-    	template <size_t Kd>
-    	static constexpr auto EuclideanDistance(const Coord<size_t, Kd>& _A, const Coord<size_t, Kd>& _B) {
-        	return sqrtf(SqrEuclideanDistance(_A, _B));
+         * @tparam Kd The number of dimensions of the nodes.
+         * @param _A The first node.
+         * @param _B The second node.
+         * @return The Euclidean distance between _A and _B.
+         */
+        template<size_t Kd>
+        static constexpr auto EuclideanDistance(const Coord<size_t, Kd>& _A, const Coord<size_t, Kd>& _B) {
+            return sqrtf(SqrEuclideanDistance(_A, _B));
         }
 
-    	/*
-		 * @brief Computes the squared Euclidean distance between two nodes.
-		 *
-		 * @tparam Kd The number of dimensions of the nodes.
-		 * @param _A The first node.
-		 * @param _B The second node.
-		 * @return The squared Euclidean distance between _A and _B.
-		 */
-    	template <size_t Kd>
-    	static constexpr auto SqrEuclideanDistance(const Coord<size_t, Kd>& _A, const Coord<size_t, Kd>& _B) {
+        /*
+         * @brief Computes the squared Euclidean distance between two nodes.
+         *
+         * @tparam Kd The number of dimensions of the nodes.
+         * @param _A The first node.
+         * @param _B The second node.
+         * @return The squared Euclidean distance between _A and _B.
+         */
+        template<size_t Kd>
+        static constexpr auto SqrEuclideanDistance(const Coord<size_t, Kd>& _A, const Coord<size_t, Kd>& _B) {
+            float result = 0.0F;
 
-			float result = 0.0F;
+            for (size_t i = 0U; i < Kd; ++i) {
+                auto val = _B[i] - _A[i];
+                result += val * val;
+            }
 
-        	for (size_t i = 0U; i < Kd; ++i) {
-
-				auto val = _B[i] - _A[i];
-        		result += val * val;
-        	}
-
-        	return result;
+            return result;
         }
 
-		/**
-		  * @brief Calculate the Manhattan distance between two nodes.
-		  *
-		  * @tparam Kd The number of dimensions of the nodes.
-		  * @param _A The first node.
-		  * @param _B The second node.
-		  * @return The Manhattan distance between _A and _B.
-		  */
-    	template <size_t Kd>
-    	static constexpr auto ManhattanDistance(const Coord<size_t, Kd>& _A, const Coord<size_t, Kd>& _B) {
+        /**
+          * @brief Calculate the Manhattan distance between two nodes.
+          *
+          * @tparam Kd The number of dimensions of the nodes.
+          * @param _A The first node.
+          * @param _B The second node.
+          * @return The Manhattan distance between _A and _B.
+          */
+        template<size_t Kd>
+        static constexpr auto ManhattanDistance(const Coord<size_t, Kd>& _A, const Coord<size_t, Kd>& _B) {
+            float result = 0.0F;
 
-        	float result = 0.0F;
+            for (size_t i = 0U; i < Kd; ++i) {
+                result += abs(_B[i] - _A[i]);
+            }
 
-        	for (size_t i = 0U; i < Kd; ++i) {
-        		result += abs(_B[i] - _A[i]);
-        	}
-
-        	return result;
+            return result;
         }
 
 #pragma endregion Heuristics
-
     };
 } // CHDR::Solvers
 
