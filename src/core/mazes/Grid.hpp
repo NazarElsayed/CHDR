6c765e8f11c7d0f32c6f6441249ab9e239b6d195
wolgemoth <25903926+wolgemoth@users.noreply.github.com>
2024-07-26 13:08:33 +0100

    Fix Grid.At()

--- src/core/mazes/Grid.hpp
+++ src/core/mazes/Grid.hpp
@@ -68,7 +68,7 @@ namespace CHDR::Mazes {
         }
 
         template<typename... Args>
-        [[nodiscard]] constexpr Node<T>& At(const Args&... _coord) const {
+        [[nodiscard]] constexpr Node<T>& At(const Args&... _coord) {
 
             const size_t index = Utils::To1D({ _coord... }, m_Size);
 
@@ -79,7 +79,7 @@ namespace CHDR::Mazes {
 #endif // NDEBUG
         }
 
-        [[nodiscard]] constexpr Node<T>& At(const coord_t& _coord) const {
+        [[nodiscard]] constexpr Node<T>& At(const coord_t& _coord) {
 
             const size_t index = Utils::To1D(_coord, m_Size);
 
@@ -91,7 +91,7 @@ namespace CHDR::Mazes {
         }
 
         template<typename... Args>
-        [[nodiscard]] constexpr Node<T>& At(const size_t& _index) const {
+        [[nodiscard]] constexpr Node<T>& At(const size_t& _index) {
 
 #ifndef NDEBUG
             return m_Nodes.at(_index);
--- src/test/scripts/tests/AStar.hpp
+++ src/test/scripts/tests/AStar.hpp
@@ -28,6 +28,14 @@ namespace Test::Tests {
             auto maze = CHDR::Mazes::Grid<Kd, T>(size);
             maze.Nodes(Generator::Generate<T>(start, end, 0U, size));
 
+            auto& start1 = maze.At(start);
+            start1.Value(true);
+
+            auto& start2 = maze.At(start);
+            start2.Value(false);
+
+            Debug::Log(std::to_string(start1.Value()) + " == " + std::to_string(start2.Value()));
+
             // Display the maze:
             Generator::Display(start, end, size, maze);
 
