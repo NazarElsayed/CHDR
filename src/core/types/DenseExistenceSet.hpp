8051d1d89faea3c6d3756da0b889f5047c65bcc1
wolgemoth <25903926+wolgemoth@users.noreply.github.com>
2024-08-07 23:12:43 +0100

    Pretty, Cleanup, and Document DenseExistenceSet.hpp

--- src/core/types/DenseExistenceSet.hpp
+++ src/core/types/DenseExistenceSet.hpp
@@ -5,36 +5,40 @@
 
 namespace CHDR {
 
-    struct LowestMemoryUsage  {};
-    struct LowMemoryUsage     {};
-    struct Balanced           {};
-    struct HighestPerformance {};
+    struct LowestMemoryUsage  {}; /** @brief Each item is represented by 1 bit in memory. */
+    struct LowMemoryUsage     {}; /** @brief Each item is represented by 8 bits in memory. */
+    struct Balanced           {}; /** @brief Each item is represented by 32 bits in memory. */
+    struct HighestPerformance {}; /** @brief Each item is represented by the system size of a DWORD in memory. */
 
     template<typename T>
     struct Alignment {};
+    template<> struct Alignment<LowestMemoryUsage>  { using Type =      bool; };
+    template<> struct Alignment<LowMemoryUsage>     { using Type =      char; };
+    template<> struct Alignment<Balanced>           { using Type =  uint32_t; };
+    template<> struct Alignment<HighestPerformance> { using Type = uintptr_t; };
 
-    template<>
-    struct Alignment<LowestMemoryUsage> {
-        using Type = bool;
-    };
-
-    template<>
-    struct Alignment<LowMemoryUsage> {
-        using Type = char;
-    };
-
-    template<>
-    struct Alignment<Balanced> {
-        using Type = int32_t;
-    };
-
-    template<>
-    struct Alignment<HighestPerformance> {
-        using Type = intptr_t;
-    };
-
+    /**
+     * @class DenseExistenceSet
+     * @brief A set allowing for efficient existence checks without needing to store the original data in memory.
+     *
+     * @tparam AlignmentType The alignment type used by the set.
+     *
+     * @see LowestMemoryUsage
+     * @see LowMemoryUsage
+     * @see Balanced
+     * @see HighestPerformance
+     */
     template <typename AlignmentType = LowestMemoryUsage>
     class DenseExistenceSet {
+
+        static_assert(
+            std::is_same_v<AlignmentType, LowestMemoryUsage>  ||
+            std::is_same_v<AlignmentType, LowMemoryUsage>     ||
+            std::is_same_v<AlignmentType, Balanced>           ||
+            std::is_same_v<AlignmentType, HighestPerformance>,
+            "AlignmentType must be one of the following: "
+            "LowestMemoryUsage, LowMemoryUsage, Balanced, HighestPerformance"
+        );
 
     private:
 
@@ -45,15 +49,15 @@ namespace CHDR {
     public:
 
         /**
-         * @brief Initialise DenseExistenceSet.
+         * @brief Initialise set.
          * @param[in] _capacity Initial capacity of the set. Must be larger than 0.
          */
-        constexpr DenseExistenceSet(const size_t& _capacity = 1U) {
+        constexpr explicit DenseExistenceSet(const size_t& _capacity = 1U) {
             m_Bits.resize(_capacity);
         }
 
         /**
-         * @brief Initialise DenseExistenceSet using a collection of items.
+         * @brief Initialise set using a collection of items.
          * @details Please note: Duplicate entries will be merged.
          *
          * @param[in] _items Items to construct the set using.
@@ -74,6 +78,10 @@ namespace CHDR {
             }
         }
 
+        /**
+         * @brief Add a hash to the set.
+         * @param[in] _hash The hash value to be added.
+         */
         void Add(const size_t& _hash) {
 
             if (_hash >= m_Bits.size()) {
@@ -83,6 +91,14 @@ namespace CHDR {
             m_Bits[_hash] = static_cast<boolean_t>(true);
         }
 
+        /**
+         * @brief Remove a hash from the set.
+         * @param[in] _hash The hash value to be removed.
+         * @note Please note that this function does not resize the set.
+         *
+         * @see set::Trim()
+         * @see set::Clear()
+         */
         void Remove(const size_t& _hash) {
 
             if (_hash < m_Bits.size()) {
@@ -90,10 +106,20 @@ namespace CHDR {
             }
         }
 
+        /**
+         * @brief Check if the given hash exists in the set.
+         * @param[in] _hash The hash value to check.
+         * @return True if the hash exists in the set, false otherwise.
+         */
         [[nodiscard]] inline bool Contains(const size_t& _hash) const {
             return _hash < m_Bits.size() && static_cast<bool>(m_Bits[_hash]);
         }
 
+        /**
+         * @brief Trim the set by removing trailing false values.
+         *
+         * @see set::Clear()
+         */
         void Trim() {
 
             const auto it = std::find_if(
@@ -112,10 +138,20 @@ namespace CHDR {
             }
         }
 
+        /**
+         * @brief Clear the content of the set.
+         * @details Remove all elements from the set.
+         */
         void Clear() {
             m_Bits.clear();
         }
 
+        /**
+         * @brief Get the size of the set.
+         * @details Returns the number of elements in the set.
+         *
+         * @return The size of the set.
+         */
         constexpr auto Size() const {
             return m_Bits.size();
         }
