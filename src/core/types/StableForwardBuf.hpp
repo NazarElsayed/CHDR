d9b3241d2a5e97be97b29f0f145def57e866fcb1
wolgemoth <25903926+wolgemoth@users.noreply.github.com>
2024-10-24 10:07:27 +0100

    StableForwardBuf.hpp Reformat

--- src/core/types/StableForwardBuf.hpp
+++ src/core/types/StableForwardBuf.hpp
@@ -11,8 +11,11 @@ namespace CHDR {
 
     template <typename T, size_t BlockWidth = 1024U>
     class StableForwardBuf {
+
     private:
+
         using Block = std::unique_ptr<T[]>;
+
         size_t m_Index;
         std::forward_list<Block> m_Blocks;
 
@@ -22,7 +25,9 @@ namespace CHDR {
         }
 
         constexpr StableForwardBuf(const std::initializer_list<size_t>& _items) : m_Index(0U), m_Blocks() {
+
             m_Blocks.emplace_front(std::make_unique<T[]>(BlockWidth));
+
             for (const auto& item : _items) {
                 Push(item);
             }
@@ -34,16 +39,20 @@ namespace CHDR {
         }
 
         T& Push(const T& _item) {
+
             if (m_Index >= BlockWidth) {
                 Expand();
             }
+
             return m_Blocks.front()[m_Index++] = _item;
         }
 
         T& Emplace(T&& _item) {
+
             if (m_Index >= BlockWidth) {
                 Expand();
             }
+
             return m_Blocks.front()[m_Index++] = std::move(_item);
         }
 
@@ -54,75 +63,83 @@ namespace CHDR {
         template<bool Const>
         class StableIterator {
         private:
-            using BlockIterator = typename std::conditional<
-                Const,
+
+            using BlockIterator = typename std::conditional<Const,
                 typename std::forward_list<Block>::const_iterator,
                 typename std::forward_list<Block>::iterator
             >::type;
 
-            using ElementIterator = typename std::conditional<
-                Const,
-                const T*,
-                T*
-            >::type;
+            using ElementIterator = typename std::conditional<Const, const T*, T*>::type;
 
             BlockIterator block_iter;
             BlockIterator block_end;
+
             ElementIterator element_iter;
+
             size_t remaining_elements;
 
         public:
+
             using iterator_category = std::forward_iterator_tag;
-            using value_type = T;
+
+            using      value_type = T;
             using difference_type = std::ptrdiff_t;
-            using pointer = typename std::conditional<Const, const T*, T*>::type;
+
+            using   pointer = typename std::conditional<Const, const T*, T*>::type;
             using reference = typename std::conditional<Const, const T&, T&>::type;
 
-            StableIterator(BlockIterator b_iter, BlockIterator b_end, size_t remaining_elem)
-                : block_iter(b_iter), block_end(b_end), remaining_elements(remaining_elem) {
+            StableIterator(BlockIterator b_iter, BlockIterator b_end, size_t remaining_elem) :
+                block_iter(b_iter),
+                block_end(b_end),
+                remaining_elements(remaining_elem)
+            {
                 if (block_iter != block_end) {
                     element_iter = block_iter->get();
                 }
             }
 
-            StableIterator &operator++() {
+            StableIterator &operator ++() {
+
                 ++element_iter;
                 --remaining_elements;
+
                 if ((element_iter - block_iter->get()) == BlockWidth || !remaining_elements) {
+
                     ++block_iter;
                     if (block_iter != block_end) {
                         element_iter = block_iter->get();
                     }
                 }
+
                 return *this;
             }
 
-            StableIterator operator++(int) {
+            StableIterator operator ++(int) {
                 auto tmp = *this;
                 ++(*this);
                 return tmp;
             }
 
             reference operator* () const { return *element_iter; }
-            pointer operator->() const { return element_iter; }
+              pointer operator->() const { return  element_iter; }
 
-            bool operator==(const StableIterator &other) const {
+            bool operator == (const StableIterator &other) const {
                 return block_iter == other.block_iter &&
                     (block_iter == block_end || (element_iter == other.element_iter && remaining_elements == other.remaining_elements));
             }
 
-            bool operator!=(const StableIterator &other) const { return !(*this == other); }
+            bool operator != (const StableIterator &other) const { return !(*this == other); }
         };
 
-        using iterator = StableIterator<false>;
+        using       iterator = StableIterator<false>;
         using const_iterator = StableIterator<true>;
 
-        iterator begin() { return iterator(m_Blocks.begin(), m_Blocks.end(), m_Index); }
-        const_iterator begin() const { return const_iterator(m_Blocks.begin(), m_Blocks.end(), m_Index); }
+              iterator  begin()       { return       iterator(m_Blocks.begin(), m_Blocks.end(), m_Index); }
+        const_iterator  begin() const { return const_iterator(m_Blocks.begin(), m_Blocks.end(), m_Index); }
         const_iterator cbegin() const { return const_iterator(m_Blocks.begin(), m_Blocks.end(), m_Index); }
 
-        iterator end() { return iterator(m_Blocks.end(), m_Blocks.end(), 0U); }
-        const_iterator end() const { return const_iterator(m_Blocks.end(), m_Blocks.end(), 0U); }
+              iterator  end()       { return       iterator(m_Blocks.end(), m_Blocks.end(), 0U); }
+        const_iterator  end() const { return const_iterator(m_Blocks.end(), m_Blocks.end(), 0U); }
         const_iterator cend() const { return const_iterator(m_Blocks.end(), m_Blocks.end(), 0U); }
     };
 
