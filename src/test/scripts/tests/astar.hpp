9d71f87ab00b9b1d1e3c5f9d420441733c5f06b4
wolgemoth <25903926+wolgemoth@users.noreply.github.com>
2024-12-11 17:28:49 +0000

    New Testing Methodology

--- src/test/scripts/tests/astar.hpp
+++ src/test/scripts/tests/astar.hpp
@@ -44,16 +44,13 @@ namespace test::tests {
             using coord_t = chdr::coord<index_t, Kd>;
 
             // Test parameters:
+            const size_t test_samples = std::max(100000000UL / chdr::utils::product<size_t>(_dimensions), 1UL);
+
             constexpr size_t seed(0U);
 
             const     coord_t size = _dimensions;
             constexpr coord_t start {};
                       coord_t end;
-
-            /* GENERATE MAZE */
-            const auto grid = generator::grid::generate<weight_t>(start, end, 0.0, 0.0, seed, size);
-            //const auto graph = chdr::mazes::graph<index_t, scalar_t>(grid);
-            //const auto graph = generator::graph::generate<weight_t, index_t, scalar_t>(start, end, 0.0, 0.0, seed, size);
 
             /* MAX DRAW SIZE */
             const bool drawable (
@@ -63,21 +60,46 @@ namespace test::tests {
                       Kd <    3U
             );
 
-            const auto sw_start = std::chrono::high_resolution_clock::now();
+            /* GENERATE MAZE */
 
-            auto solver = chdr::solvers::astar<weight_t, Kd, scalar_t, index_t>();
-            auto path   = solver.solve(grid, start, end, HEURISTIC);
-            //auto path   = solver.solve(graph, start, end, size, HEURISTIC);
+            const auto grid = generator::grid::generate<weight_t>(start, end, 0.0, 0.0, seed, size);
+            //const auto graph = chdr::mazes::graph<index_t, scalar_t>(grid);
+            //const auto graph = generator::graph::generate<weight_t, index_t, scalar_t>(start, end, 0.0, 0.0, seed, size);
 
-            const auto pathfinding_log = "\t" + std::string(path.size() != 0U ? "[SOLVED]" : "[IMPOSSIBLE]") + "\t(~" +
-                chdr::utils::to_string(std::chrono::duration_cast<std::chrono::duration<long double>>(std::chrono::high_resolution_clock::now() - sw_start).count()) + ")";
+            /* CAPTURE SYSTEM NOISE */
+
+            auto noise_floor_min = std::numeric_limits<long double>::infinity();
+            for (size_t i = 0U; i < test_samples; ++i) {
+
+                auto sw_start = std::chrono::high_resolution_clock::now();
+                noise_floor_min = std::min(noise_floor_min, std::chrono::duration_cast<std::chrono::duration<long double>>(std::chrono::high_resolution_clock::now() - sw_start).count());
+            }
+
+            /* TEST ALGORITHM */
+
+            std::vector<coord_t> path;
+
+            auto result = std::numeric_limits<long double>::infinity();
+            for (size_t i = 0U; i < test_samples; ++i) {
+
+                auto sw_start = std::chrono::high_resolution_clock::now();
+
+                auto solver = chdr::solvers::astar<weight_t, Kd, scalar_t, index_t>();
+                path = solver.solve(grid, start, end, HEURISTIC);
+                //path = solver.solve(graph, start, end, size, HEURISTIC);
+
+                result = std::min(result, std::chrono::duration_cast<std::chrono::duration<long double>>(std::chrono::high_resolution_clock::now() - sw_start).count());
+            }
 
             if (drawable) {
                 display<weight_t, Kd>::draw_maze(start, end, size, grid, path);
             }
 
             debug::log("(A*):");
-            debug::log(pathfinding_log);
+            debug::log("\t" + std::string(path.size() != 0U ? "[SOLVED]" : "[IMPOSSIBLE]") +
+                "\t(<= ~" + chdr::utils::to_string(std::max(result - noise_floor_min, std::numeric_limits<long double>::epsilon())) + ") / "
+                     "(~" + chdr::utils::to_string((result - noise_floor_min) / static_cast<long double>(path.size())) + "/n)");
+
 
             #undef HEURISTIC
         }
