7c4a6a5df9c734b8dce045f8b30ebcfb8b83cf69
wolgemoth <25903926+wolgemoth@users.noreply.github.com>
2024-12-19 14:03:25 +0000

    Experimental Pointer Tagging

--- src/core/solvers/base/managed_node.hpp
+++ src/core/solvers/base/managed_node.hpp
@@ -17,32 +17,86 @@
 
 namespace chdr::solvers {
 
+    template <typename T>
+    struct tagged_ptr {
+
+    private:
+
+#define TAGGED
+
+#ifdef TAGGED
+
+        uintptr_t m_data_union;
+
+#else //!TAGGED
+
+        T* RESTRICT m_ptr;
+        uint16_t m_tag;
+
+#endif //!TAGGED
+
+    public:
+
+#ifdef TAGGED
+
+        constexpr tagged_ptr() noexcept : m_data_union(0U) {}
+
+        constexpr tagged_ptr(T* RESTRICT _parent) noexcept : m_data_union(reinterpret_cast<uintptr_t>(_parent)) {}
+
+        [[nodiscard]] constexpr T* ptr() const noexcept {
+            return reinterpret_cast<T*>(m_data_union & ((1ULL << 48U) - 1U));
+        }
+
+        constexpr void ptr(T* RESTRICT _parent) noexcept {
+            m_data_union = (m_data_union & ~((1ULL << 48U) - 1U)) | reinterpret_cast<uintptr_t>(_parent);
+        }
+
+        [[nodiscard]] constexpr uint16_t tag() const noexcept {
+            return static_cast<uint16_t>(m_data_union >> 48U);
+        }
+
+        constexpr void tag(const uint16_t& _value) noexcept {
+            m_data_union = (m_data_union & ((1ULL << 48U) - 1U)) | (static_cast<uintptr_t>(_value) << 48U);
+        }
+
+#else //!TAGGED
+
+        constexpr tagged_ptr() noexcept :
+            m_ptr(nullptr),
+            m_tag(0U) {}
+
+        constexpr tagged_ptr(T* RESTRICT _parent) noexcept :
+            m_ptr(_parent),
+            m_tag(0U) {}
+
+        [[nodiscard]] constexpr T* ptr() const  noexcept { return m_ptr;    }
+        constexpr void ptr(T* RESTRICT _parent) noexcept { m_ptr = _parent; }
+
+        [[nodiscard]] constexpr uint16_t tag() const noexcept { return m_tag;   }
+        constexpr void tag(const uint16_t& _value)   noexcept { m_tag = _value; }
+
+#endif //!TAGGED
+
+    };
+
     template<typename index_t>
     struct managed_node : bnode<index_t> {
 
-        managed_node* RESTRICT m_parent;
-        unsigned char m_successors;
+        tagged_ptr<managed_node> m_counter;
 
-        /**
-         * @brief Constructs an uninitialized ASNode.
-         *
-         * This constructor creates an ASNode with uninitialized members.
-         */
-        [[nodiscard]] constexpr managed_node() noexcept : // NOLINT(*-pro-type-member-init, *-use-equals-default)
-            m_parent     (nullptr),
-            m_successors (0U     ) {}
+        // ReSharper disable once CppPossiblyUninitializedMember
+        [[nodiscard]] constexpr managed_node() noexcept : m_counter() {}  // NOLINT(*-pro-type-member-init, *-use-equals-default)
 
         [[nodiscard]] constexpr managed_node(const index_t& _index, managed_node* RESTRICT const _parent = nullptr) noexcept : bnode<index_t>(_index),
-            m_parent     (_parent),
-            m_successors (0U     )
+            m_counter(_parent)
         {
             incr();
         }
 
         [[nodiscard]] constexpr managed_node(const managed_node& _other) noexcept : bnode<index_t>(_other.m_index),
-            m_parent     (_other.m_parent),
-            m_successors (0U             )
+            m_counter(_other.m_counter)
         {
+            m_counter.tag(0U);
             incr();
         }
 
@@ -54,19 +108,14 @@ namespace chdr::solvers {
 
             bnode<index_t>::operator=(_other.m_index);
 
-            m_parent     = _other.m_parent;
-            m_successors = _other.m_successors;
+            m_counter = _other.m_counter;
 
             return *this;
         }
 
         [[nodiscard]] constexpr managed_node(managed_node&& _other) noexcept : bnode<index_t>(std::move(_other.m_index)),
-            m_parent     (_other.m_parent    ),
-            m_successors (_other.m_successors)
-        {
-            _other.m_parent     = nullptr;
-            _other.m_successors = 0U;
-        }
+            m_counter(_other.m_counter)
+        {}
 
         constexpr managed_node& operator=(managed_node&& _other) noexcept {
 
@@ -76,11 +125,7 @@ namespace chdr::solvers {
 
             bnode<index_t>::operator=(std::move(_other.m_index));
 
-            m_parent     = _other.m_parent;
-            m_successors = _other.m_successors;
-
-            _other.m_parent     = nullptr;
-            _other.m_successors = 0U;
+            m_counter = std::move(_other.m_counter);
 
             return *this;
         }
@@ -91,17 +136,17 @@ namespace chdr::solvers {
 
         void expunge() noexcept {
 
-            while (m_parent != nullptr) {
+            while (m_counter.ptr() != nullptr) {
 
                 decr();
 
-                if (m_parent->m_successors == 0U) {
+                if (m_counter.tag() == 0U) {
 
-                    auto* temp = m_parent;
-                    m_parent = temp->m_parent;
+                    auto temp = m_counter;
+                    m_counter = temp.ptr()->m_counter;
 
-                    temp->m_parent = nullptr;
-                    delete temp;
+                    temp.ptr(nullptr);
+                    delete temp.ptr();
                 }
                 else {
                     break;
@@ -110,21 +155,21 @@ namespace chdr::solvers {
         }
 
         constexpr void decr() noexcept {
-            if (m_parent != nullptr) {
-                assert(m_parent->m_successors != static_cast<decltype(m_successors)>(0U) && "Underflow detected!");
-                m_parent->m_successors -= 1U;
+            if (m_counter.ptr() != nullptr) {
+                assert(m_counter.tag() != static_cast<decltype(m_counter.tag())>(0U) && "Underflow detected!");
+                m_counter.tag(m_counter.tag() - 1U);
             }
         }
 
         constexpr void incr() noexcept {
-            if (m_parent != nullptr) {
-                assert(m_parent->m_successors != static_cast<decltype(m_successors)>(-1U) && "Overflow detected!");
-                m_parent->m_successors += 1U;
+            if (m_counter.ptr() != nullptr) {
+                assert(m_counter.tag() != static_cast<decltype(m_counter.tag())>(-1U) && "Overflow detected!");
+                m_counter.tag(m_counter.tag() + 1U);
             }
         }
 
-        template<typename node_t, typename coord_t>
-        auto backtrack(const coord_t& _size, const size_t& _capacity = 0U) const {
+        template <typename node_t, typename coord_t>
+        [[nodiscard]] auto backtrack(const coord_t& _size, const size_t& _capacity = 0U) const {
 
             static_assert(std::is_base_of_v<managed_node, node_t>, "node_t must derive from managed_node");
 
@@ -133,7 +178,7 @@ namespace chdr::solvers {
             result.reserve(_capacity);
 
             // Recurse from end node to start node, inserting into a result buffer:
-            for (const auto* t = this; t->m_parent != nullptr; t = static_cast<const node_t*>(t->m_parent)) {
+            for (const auto* t = this; t->m_counter.ptr() != nullptr; t = static_cast<const node_t*>(t->m_counter.ptr())) {
                 result.emplace_back(utils::to_nd(t->m_index, _size));
             }
 
@@ -141,12 +186,6 @@ namespace chdr::solvers {
             std::reverse(result.begin(), result.end());
 
             return result;
-        }
-
-        [[nodiscard]] friend constexpr bool operator < (const managed_node& _a, const managed_node& _b) noexcept {
-            return _a.m_fScore == _b.m_fScore ?
-                   _a.m_gScore >  _b.m_gScore :
-                   _a.m_fScore >  _b.m_fScore;
         }
     };
 
