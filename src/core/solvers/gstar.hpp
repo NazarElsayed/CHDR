367b44b09b3e0651bec33f2b72e3b4288d081ccb
wolgemoth <25903926+wolgemoth@users.noreply.github.com>
2025-01-03 21:01:27 +0000

    G* Heap Sorting Fix

--- src/core/solvers/gstar.hpp
+++ src/core/solvers/gstar.hpp
@@ -101,6 +101,8 @@ namespace chdr::solvers {
 
                     if (curr_ptr == nullptr) {
                         curr.expunge();
+                    }
+                    else {
                         _open.reheapify(_open.back());
                     }
                 }
