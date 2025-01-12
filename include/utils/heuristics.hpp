db8ee5707984690b00612d74058a28f8174e0ffc
wolgemoth <25903926+wolgemoth@users.noreply.github.com>
2025-01-12 13:23:43 +0000

    Implement Additional Heuristics

--- include/utils/heuristics.hpp
+++ include/utils/heuristics.hpp
@@ -74,6 +74,92 @@ namespace chdr {
             return result;
         }
 
+
+        /**
+         * @brief Computes the Chebyshev distance between two nodes.
+         *
+         * @param _a The first node.
+         * @param _b The second node.
+         * @return The Chebyshev distance between _a and _b.
+         */
+        template <typename scalar_t, typename coord_t>
+        [[maybe_unused, nodiscard]] static constexpr auto chebyshev_distance(const coord_t& _a, const coord_t& _b) noexcept {
+	        constexpr auto Kd = std::tuple_size_v<std::decay_t<coord_t>>;
+
+	        scalar_t result{0};
+
+	        IVDEP
+	        for (size_t i = 0U; i < Kd; ++i) {
+
+		        const auto val = static_cast<scalar_t>(utils::abs(static_cast<signed>(_b[i]) - static_cast<signed>(_a[i])));
+
+	        	if (val > result) {
+			        result = val;
+		        }
+	        }
+
+	        return result;
+        }
+
+        /**
+         * @brief Computes the Canberra distance between two nodes.
+         *
+         * @param _a The first node.
+         * @param _b The second node.
+         * @return The Canberra distance between _a and _b.
+         */
+        template <typename scalar_t, typename coord_t>
+        [[maybe_unused, nodiscard]] static constexpr auto canberra_distance(const coord_t& _a, const coord_t& _b) noexcept {
+
+	    	constexpr auto Kd = std::tuple_size_v<std::decay_t<coord_t>>;
+
+	        scalar_t result{0};
+
+	        IVDEP
+	        for (size_t i = 0U; i < Kd; ++i) {
+
+	        	const auto num   = static_cast<scalar_t>(utils::abs(static_cast<signed>(_b[i]) - static_cast<signed>(_a[i])));
+		        const auto denom = static_cast<scalar_t>(utils::abs(_a[i]) + utils::abs(_b[i]));
+
+	        	if (denom != 0) {
+			        result += num / denom;
+		        }
+	        }
+
+	        return result;
+        }
+
+        /**
+         * @brief Computes the Cosine distance between two nodes.
+         *
+         * @param _a The first node.
+         * @param _b The second node.
+         * @return The Cosine distance (1 - Cosine similarity) between _a and _b.
+         */
+        template <typename scalar_t, typename coord_t>
+        [[maybe_unused, nodiscard]] static constexpr auto cosine_distance(const coord_t& _a, const coord_t& _b) noexcept {
+
+	    	constexpr auto Kd = std::tuple_size_v<std::decay_t<coord_t>>;
+
+	        scalar_t dot_product {0};
+	        scalar_t norm_a      {0};
+	        scalar_t norm_b      {0};
+
+	        IVDEP
+	        for (size_t i = 0; i < Kd; ++i) {
+	        	
+		        const scalar_t val_a = static_cast<scalar_t>(_a[i]);
+		        const scalar_t val_b = static_cast<scalar_t>(_b[i]);
+	        	
+		        dot_product += val_a * val_b;
+		        norm_a      += val_a * val_a;
+		        norm_b      += val_b * val_b;
+	        }
+
+	        const auto norm_product = utils::sqrt(norm_a) * utils::sqrt(norm_b);
+	    	
+	        return (norm_product > 0) ? (1 - (dot_product / norm_product)) : static_cast<scalar_t>(1);
+        }
 	};
 
 } //chdr
