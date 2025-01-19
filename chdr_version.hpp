08b623824ad2b7ba7a6b9544ce13425f1c7adb7a
wolgemoth <25903926+wolgemoth@users.noreply.github.com>
2025-01-19 10:03:59 +0000

    Add Include Guard to Version Header

--- chdr_version.hpp
+++ chdr_version.hpp
@@ -6,9 +6,14 @@
  * https://creativecommons.org/licenses/by-nc-nd/4.0/
  */
 
+#ifndef CHDR_VERSION_HPP
+#define CHDR_VERSION_HPP
+
 #define CHDR_VERSION          "0.1.0-alpha+x86_64-Linux-native"
 #define CHDR_VERSION_MAJOR     0
 #define CHDR_VERSION_MINOR     1
 #define CHDR_VERSION_PATCH     0
 #define CHDR_VERSION_TWEAK    "alpha"
 #define CHDR_VERSION_METADATA "x86_64-Linux-native"
+
+#endif //CHDR_VERSION_HPP
