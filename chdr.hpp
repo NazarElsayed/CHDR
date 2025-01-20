85b505103f9f4ba2bd24ab8c56dfc47330296258
wolgemoth <25903926+wolgemoth@users.noreply.github.com>
2025-01-20 21:58:08 +0000

    Optimise Location of Code Paths in Binary

--- chdr.hpp
+++ chdr.hpp
@@ -9,12 +9,12 @@
 #ifndef CHDR_COMMON_HPP
 #define CHDR_COMMON_HPP
 
-#define CHDR_VERSION          "0.1.0-dev+6e568d4"
+#define CHDR_VERSION          "0.1.0-dev+10f05f5"
 #define CHDR_VERSION_MAJOR     0
 #define CHDR_VERSION_MINOR     1
 #define CHDR_VERSION_PATCH     0
 #define CHDR_VERSION_TWEAK    "dev"
-#define CHDR_VERSION_METADATA "6e568d4"
+#define CHDR_VERSION_METADATA "10f05f5"
 
 /* ReSharper enable CppUnusedIncludeDirective */
 // NOLINTBEGIN(*-include-cleaner)
