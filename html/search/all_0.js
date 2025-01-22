14e43d74dd2dd53cc1ae4c3d2d96e2caa6d64893
wolgemoth <25903926+wolgemoth@users.noreply.github.com>
2025-01-22 17:12:09 +0000

    Remove Junk

--- html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['background_0',['Background',['../md_README.html#autotoc_md2',1,'']]]
-];
--- html/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['introduction_0',['Introduction',['../md_README.html',1,'']]]
-];
