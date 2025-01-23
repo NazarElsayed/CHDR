3acd111c9e8b003f3bc20ad06d3bd8681d564432
wolgemoth <25903926+wolgemoth@users.noreply.github.com>
2025-01-23 18:52:18 +0000

    Updated Documentation Style

--- chdr.hpp
+++ chdr.hpp
@@ -9,12 +9,12 @@
 #ifndef CHDR_COMMON_HPP
 #define CHDR_COMMON_HPP
 
-#define CHDR_VERSION          "0.1.0-dev+bc69c50"
+#define CHDR_VERSION          "0.1.0-dev+163a1ba"
 #define CHDR_VERSION_MAJOR     0
 #define CHDR_VERSION_MINOR     1
 #define CHDR_VERSION_PATCH     0
 #define CHDR_VERSION_TWEAK    "dev"
-#define CHDR_VERSION_METADATA "bc69c50"
+#define CHDR_VERSION_METADATA "163a1ba"
 
 /* ReSharper enable CppUnusedIncludeDirective */
 // NOLINTBEGIN(*-include-cleaner)
