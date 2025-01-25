0443d6d805a7ecc9619eb8cbf6397eaf5811efa8
wolgemoth <25903926+wolgemoth@users.noreply.github.com>
2025-01-25 17:35:36 +0000

    Updated Documentation

--- docs/api/search/all_7.js
+++ docs/api/search/all_7.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['id_0',['id',['../db/d4a/classchdr_1_1mazes_1_1id__node_a81af35dcfa30f8ceaffb56ca11327648.html#a81af35dcfa30f8ceaffb56ca11327648',1,'chdr::mazes::id_node::id() const noexcept'],['../db/d4a/classchdr_1_1mazes_1_1id__node_a7244d075866ff55bc57ce4a06e524576.html#a7244d075866ff55bc57ce4a06e524576',1,'chdr::mazes::id_node::id(index_t _id) noexcept']]],
-  ['id_5fnode_1',['id_node',['../db/d4a/classchdr_1_1mazes_1_1id__node.html',1,'id_node&lt; index_t &gt;'],['../db/d4a/classchdr_1_1mazes_1_1id__node_a8c49e8270e35716ab47e4edb5084567b.html#a8c49e8270e35716ab47e4edb5084567b',1,'chdr::mazes::id_node::id_node()']]],
+  ['id_0',['id',['../db/d4a/classchdr_1_1mazes_1_1id__node_a5e291e37a8816b6866a8873326ea7bd6.html#a5e291e37a8816b6866a8873326ea7bd6',1,'chdr::mazes::id_node::id() const noexcept'],['../db/d4a/classchdr_1_1mazes_1_1id__node_aef3937c098aad28896108c719b3b1041.html#aef3937c098aad28896108c719b3b1041',1,'chdr::mazes::id_node::id(index_t _id) noexcept']]],
+  ['id_5fnode_1',['id_node',['../db/d4a/classchdr_1_1mazes_1_1id__node.html',1,'id_node&lt; index_t &gt;'],['../db/d4a/classchdr_1_1mazes_1_1id__node_ae28ec69302ef54fbae1d46c0ddea7774.html#ae28ec69302ef54fbae1d46c0ddea7774',1,'chdr::mazes::id_node::id_node()']]],
   ['installation_2',['Installation',['../da/dc4/md_docs_2manual_2getting__started.html#installation',1,'']]],
   ['introduction_3',['Introduction',['../index.html',1,'']]],
-  ['is_5factive_4',['is_active',['../db/d4a/classchdr_1_1mazes_1_1id__node_a48ad40a8e9a74f9afd6e001a34d353bc.html#a48ad40a8e9a74f9afd6e001a34d353bc',1,'chdr::mazes::id_node::is_active()'],['../db/dc4/structchdr_1_1mazes_1_1weighted__node_a518756b2cf243cebed5b0d669be1f757.html#a518756b2cf243cebed5b0d669be1f757',1,'chdr::mazes::weighted_node::is_active()']]]
+  ['is_5factive_4',['is_active',['../db/d4a/classchdr_1_1mazes_1_1id__node_aef891cd178d3d35f68a9923c4d77c9e4.html#aef891cd178d3d35f68a9923c4d77c9e4',1,'chdr::mazes::id_node::is_active()'],['../db/dc4/structchdr_1_1mazes_1_1weighted__node_a7296f53fa8d3149e5cd689711e22e9c3.html#a7296f53fa8d3149e5cd689711e22e9c3',1,'chdr::mazes::weighted_node::is_active()']]]
 ];
--- docs/api/search/all_d.js
+++ docs/api/search/all_d.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['shrink_5fto_5ffit_0',['shrink_to_fit',['../d6/db9/classchdr_1_1existence__set_ad9e4af5032d71179d3abf04db463f791.html#ad9e4af5032d71179d3abf04db463f791',1,'chdr::existence_set']]],
-  ['sign_1',['sign',['../d2/dcb/structchdr_1_1utils_a478e1466e6f669f9f338a30353e7c122.html#a478e1466e6f669f9f338a30353e7c122',1,'chdr::utils']]],
-  ['size_2',['size',['../d6/db9/classchdr_1_1existence__set_a3f3028e765d0c5eba96a7f0598802f05.html#a3f3028e765d0c5eba96a7f0598802f05',1,'chdr::existence_set']]],
+  ['shrink_5fto_5ffit_0',['shrink_to_fit',['../d6/db9/classchdr_1_1existence__set_a2f7a64afff0c9c79e40c9cf475f5c0de.html#a2f7a64afff0c9c79e40c9cf475f5c0de',1,'chdr::existence_set']]],
+  ['sign_1',['sign',['../d2/dcb/structchdr_1_1utils_a95de21ed13a7027d1c07fa28404bd4c4.html#a95de21ed13a7027d1c07fa28404bd4c4',1,'chdr::utils']]],
+  ['size_2',['size',['../d6/db9/classchdr_1_1existence__set_a545aa7b6f77b13ccee4052087e9ac6a9.html#a545aa7b6f77b13ccee4052087e9ac6a9',1,'chdr::existence_set']]],
   ['solve_3',['solve',['../d3/df4/classchdr_1_1solvers_1_1solver_a096fe8cb528641fed6813df1f40bd347.html#a096fe8cb528641fed6813df1f40bd347',1,'chdr::solvers::solver::solve(Args &amp;&amp;... _args)'],['../d3/df4/classchdr_1_1solvers_1_1solver_aa62632bf43d0cf6db6a9f6de5e76b0ba.html#aa62632bf43d0cf6db6a9f6de5e76b0ba',1,'chdr::solvers::solver::solve(const params_t &amp;_params)']]],
-  ['solver_4',['solver',['../d3/df4/classchdr_1_1solvers_1_1solver.html',1,'solver&lt; solver_t, params_t &gt;'],['../d3/df4/classchdr_1_1solvers_1_1solver_afd9ac73f57f4eae43f130211d9b95d86.html#afd9ac73f57f4eae43f130211d9b95d86',1,'chdr::solvers::solver::solver()']]],
+  ['solver_4',['solver',['../d3/df4/classchdr_1_1solvers_1_1solver.html',1,'solver&lt; solver_t, params_t &gt;'],['../d3/df4/classchdr_1_1solvers_1_1solver_a458fd524cfda3f01c4c8a5d4420303bb.html#a458fd524cfda3f01c4c8a5d4420303bb',1,'chdr::solvers::solver::solver()']]],
   ['solvers_5',['solvers',['../d5/da1/md_docs_2manual_2roadmap.html#eikonal-solvers',1,'Eikonal Solvers'],['../d5/da1/md_docs_2manual_2roadmap.html#solvers',1,'Solvers']]],
-  ['sqr_5feuclidean_5fdistance_6',['sqr_euclidean_distance',['../d2/df4/structchdr_1_1heuristics_a2fe90d2468d08f5dc0157ff297b921ab.html#a2fe90d2468d08f5dc0157ff297b921ab',1,'chdr::heuristics']]],
-  ['sqrt_7',['sqrt',['../d2/dcb/structchdr_1_1utils_a4414bbea278f4bd180d1d579a4b2fcfa.html#a4414bbea278f4bd180d1d579a4b2fcfa',1,'chdr::utils']]],
+  ['sqr_5feuclidean_5fdistance_6',['sqr_euclidean_distance',['../d2/df4/structchdr_1_1heuristics_aca4234bd65c6fc6f82b35bc736d2ed0b.html#aca4234bd65c6fc6f82b35bc736d2ed0b',1,'chdr::heuristics']]],
+  ['sqrt_7',['sqrt',['../d2/dcb/structchdr_1_1utils_a4dedc1630ec2ece0da95aea738d2357b.html#a4dedc1630ec2ece0da95aea738d2357b',1,'chdr::utils']]],
   ['started_8',['Getting Started',['../da/dc4/md_docs_2manual_2getting__started.html',1,'']]]
 ];
