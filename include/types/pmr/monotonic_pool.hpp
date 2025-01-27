a8a164f064b044ce32cb85d74c01b795d028512a
wolgemoth <25903926+wolgemoth@users.noreply.github.com>
2025-01-27 08:41:21 +0000

    Better Monotonic Allocation Strategy

--- include/types/pmr/monotonic_pool.hpp
+++ include/types/pmr/monotonic_pool.hpp
@@ -20,6 +20,34 @@ namespace chdr {
 
     class monotonic_pool : public std::pmr::memory_resource {
 
+        struct block final {
+
+            size_t   size;
+            size_t   alignment;
+            uint8_t* data;
+
+            [[nodiscard]] HOT constexpr block(size_t _size, size_t _alignment, uint8_t* _data) noexcept :
+                size     (_size),
+                alignment(_alignment),
+                data     (_data) {}
+
+            ~block() = default;
+
+            [[nodiscard]] HOT constexpr block           (const block&) = default;
+            HOT constexpr block& operator=(const block&) = default;
+
+            [[nodiscard]] HOT constexpr block(block&& _other) noexcept = default;
+
+#if __cplusplus > 202302L
+            constexpr
+#endif
+            block& operator=(block&& _other) noexcept = default;
+
+            [[nodiscard]] HOT constexpr bool operator < (const block& _other) const noexcept {
+                return size < _other.size;
+            }
+        };
+
         static constexpr size_t s_initial_heap_block_size {  4096U };
         static constexpr size_t     s_max_heap_block_size { 65536U };
         static constexpr size_t        s_stack_block_size {  4096U };
@@ -31,30 +59,50 @@ namespace chdr {
         size_t m_block_write;        // Current write position in the active block.
         size_t m_active_block_index; // Index of the current active block.
 
-        struct block {
-            std::unique_ptr<uint8_t[]> data; // NOLINT(*-avoid-c-arrays)
-            size_t size;
-        };
+        std::vector<block> m_blocks;
 
-        std::vector<block> blocks;
+        HOT uint8_t* expand(size_t _bytes, size_t _alignment) {
 
-        HOT void expand(size_t _bytes) {
+            uint8_t* result = nullptr;
 
-            if (m_active_block_index + 1U < blocks.size()) { // Reuse an existing block:
-                m_current_block_size = blocks[++m_active_block_index].size;
+            try {
+
+                if (m_active_block_index + 1U < m_blocks.size()) { // Reuse an existing block:
+                    m_current_block_size = m_blocks[++m_active_block_index].size;
+                }
+                else {                                             // Allocate a new, larger block:
+
+                    m_current_block_size = utils::max(
+                        s_initial_heap_block_size,
+                        utils::max(utils::min((m_current_block_size * 3U) / 2U, s_max_heap_block_size), _bytes)
+                    );
+
+                    result = static_cast<uint8_t*>(::operator new(m_current_block_size, static_cast<std::align_val_t>(_alignment)));
+
+                    m_blocks.emplace_back(
+                        m_current_block_size,
+                        _alignment,
+                        result
+                    );
+
+                    m_active_block_index = m_blocks.size() - 1U;
+                }
+
+                m_block_write = 0U; // Reset write head to beginning.
             }
-            else {                                           // Allocate a new larger block:
+            catch (...) {
 
-                m_current_block_size = utils::max(
-                    s_initial_heap_block_size,
-                    utils::max(utils::min(m_current_block_size * 2U, s_max_heap_block_size), _bytes)
-                );
+                /* Catch any errors that occur during allocation. */
 
-                blocks.emplace_back(std::make_unique<uint8_t[]>(m_current_block_size), m_current_block_size); // NOLINT(*-avoid-c-arrays)
-                m_active_block_index = blocks.size() - 1U;
+                if (result != nullptr) {
+                    ::operator delete(result, static_cast<std::align_val_t>(_alignment));
+                    result = nullptr;
+
+                    m_blocks.pop_back();
+                }
             }
 
-            m_block_write = 0U; // Reset write head to beginning.
+            return result;
         }
 
     protected:
@@ -63,9 +111,11 @@ namespace chdr {
 
             assert(_bytes > 0U && "Allocation size must be greater than zero.");
 
+            uint8_t* aligned_ptr;
+
             // Try allocating from the stack first:
             if (m_stack_write + _bytes <= s_stack_block_size) {
-                auto* aligned_ptr = reinterpret_cast<uint8_t*>(
+                aligned_ptr = reinterpret_cast<uint8_t*>(
                     (reinterpret_cast<uintptr_t>(m_stack_block + m_stack_write) + _alignment - 1U) & ~(_alignment - 1U)
                 );
 
@@ -74,19 +124,32 @@ namespace chdr {
             }
 
             // If stack block is exhausted, fall back to dynamic blocks:
-            auto* aligned_ptr = reinterpret_cast<uint8_t*>(
-                (reinterpret_cast<uintptr_t>(blocks[m_active_block_index].data.get() + m_block_write) + _alignment - 1U) & ~(_alignment - 1U)
-            );
+            if (!m_blocks.empty()) {
 
-            if (aligned_ptr + _bytes > blocks[m_active_block_index].data.get() + blocks[m_active_block_index].size) {
+                aligned_ptr = reinterpret_cast<uint8_t*>(
+                    (reinterpret_cast<uintptr_t>(m_blocks[m_active_block_index].data + m_block_write) + _alignment - 1U) & ~(_alignment - 1U)
+                );
 
-                // Expand if the current block cannot fit the allocation.
-                expand(_bytes + _alignment);
-                return do_allocate(_bytes, _alignment);
+                // Invalidate if the current block cannot fit the allocation.
+                if (aligned_ptr + _bytes > m_blocks[m_active_block_index].data + m_blocks[m_active_block_index].size) {
+                    aligned_ptr = nullptr;
+                }
+            }
+            else {
+                aligned_ptr = nullptr;
             }
 
-            // Update write position and return the aligned pointer
-            m_block_write += static_cast<size_t>(aligned_ptr + _bytes - (blocks[m_active_block_index].data.get() + m_block_write));
+            // Expand if no valid candidate for allocation was found.
+            if (aligned_ptr == nullptr) {
+                aligned_ptr = expand(_bytes, _alignment);
+            }
+
+            // Update write position.
+            m_block_write += static_cast<size_t>(aligned_ptr + _bytes - (m_blocks[m_active_block_index].data + m_block_write));
+
+            if (aligned_ptr == nullptr) {
+                throw std::bad_alloc();
+            }
 
             return aligned_ptr;
         }
@@ -101,44 +164,67 @@ namespace chdr {
 
     public:
 
-        monotonic_pool() :
+        monotonic_pool() noexcept :
             m_current_block_size(s_initial_heap_block_size),
             m_stack_write       (0U),
             m_block_write       (0U),
-            m_active_block_index(0U)
-        {
-            expand(s_initial_heap_block_size); // Allocate the first dynamic block
+            m_active_block_index(0U) {}
+
+        ~monotonic_pool() noexcept {
+            cleanup();
         }
 
         constexpr monotonic_pool           (const monotonic_pool&) = delete;
         constexpr monotonic_pool& operator=(const monotonic_pool&) = delete;
 
-        [[nodiscard]] constexpr monotonic_pool(monotonic_pool&&) noexcept = default;
-    #if __cplusplus > 202302L
-        constexpr
-    #endif
-        monotonic_pool& operator=(monotonic_pool&&) noexcept = default;
+        [[nodiscard]] constexpr monotonic_pool(monotonic_pool&& _other) noexcept :
+            m_current_block_size(_other.m_current_block_size),
+            m_stack_write       (_other.m_stack_write       ),
+            m_block_write       (_other.m_block_write       ),
+            m_active_block_index(_other.m_active_block_index),
+            m_blocks            (std::move(_other.m_blocks) )
+        {
+            _other.m_stack_write        = 0U;
+            _other.m_current_block_size = s_initial_heap_block_size;
+            _other.m_active_block_index = 0U;
+            _other.m_block_write        = 0U;
+            _other.m_blocks.clear();
+        }
 
-        [[nodiscard]] size_t allocated() const {
+        monotonic_pool& operator=(monotonic_pool&& _other) noexcept {
 
-            // Include stack usage.
-            size_t result = s_stack_block_size;
+            if (this != &_other) {
 
-            // Include dynamic blocks.
-            for (const auto& [data, size] : blocks) {
-                result += size;
+                cleanup();
+
+                m_current_block_size  = _other.m_current_block_size;
+                m_stack_write         = _other.m_stack_write;
+                m_block_write         = _other.m_block_write;
+                m_active_block_index  = _other.m_active_block_index;
+                m_blocks              = std::move(_other.m_blocks);
+
+                _other.m_current_block_size = s_initial_heap_block_size;
+                _other.m_stack_write        = 0U;
+                _other.m_block_write        = 0U;
+                _other.m_active_block_index = 0U;
+                _other.m_blocks.clear();
             }
+            return *this;
+        }
 
-            return result;
+        void cleanup() noexcept {
+            for (const auto& item : m_blocks) {
+                ::operator delete(item.data, static_cast<std::align_val_t>(item.alignment));
+            }
         }
 
         /**
          * @brief Resets the memory resource to reuse all previously allocated memory.
          */
-        void reset() {
+        void reset() noexcept {
             m_stack_write        = 0U;
-            m_active_block_index = 0U;
             m_block_write        = 0U;
+            m_active_block_index = 0U;
         }
 
         /**
@@ -146,15 +232,14 @@ namespace chdr {
          */
         void release() {
 
-            m_stack_write        = 0U;
             m_current_block_size = s_initial_heap_block_size;
-            m_active_block_index = 0U;
+            m_stack_write        = 0U;
             m_block_write        = 0U;
+            m_active_block_index = 0U;
 
-            decltype(blocks) temp{};
-            blocks = std::move(temp);
-
-            expand(s_initial_heap_block_size);
+            cleanup();
+            decltype(m_blocks) temp{};
+            m_blocks = std::move(temp);
         }
     };
 
