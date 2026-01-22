b44f406a009e8fb91994eb2e1e49b063d5555c7c
wolgemoth <25903926+loui1024@users.noreply.github.com>
2026-01-22 13:56:07 +0000

    Latent Files

--- chdr.hpp
+++ chdr.hpp
@@ -26,7 +26,7 @@
  */
 
 /** @brief Full version string of the CHDR library. */
-#define CHDR_VERSION          "0.1.0-dev+d121e25a"
+#define CHDR_VERSION          "0.1.0-dev+831d69e2"
 
 /** @brief CHDR major version number. */
 #define CHDR_VERSION_MAJOR     0
@@ -41,7 +41,7 @@
 #define CHDR_VERSION_TWEAK    "dev"
 
 /** @brief CHDR version metadata string. */
-#define CHDR_VERSION_METADATA "d121e25a"
+#define CHDR_VERSION_METADATA "831d69e2"
 
 /** @brief Flag indicating whether or not diagnostics should be output by the library (0 = off, 1 = on). */
 #ifndef CHDR_DIAGNOSTICS
